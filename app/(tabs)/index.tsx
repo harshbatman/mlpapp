99baedeaf95e74e95783121d7bffaa37bf0908f0
harshbatman <kumarmahtoharsh7@gmail.com>
2026-02-16 23:52:36 +0530

    Add Shillong to explore cities section

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -463,6 +463,7 @@ export default function HomeScreen() {
     { name: 'Srinagar', image: require('@/assets/images/cities/srinagar.png'), color: 'standard' },
     { name: 'Shimla', image: require('@/assets/images/cities/shimla.png'), color: 'standard' },
     { name: 'Dehradun', image: require('@/assets/images/cities/dehradun.png'), color: 'standard' },
+    { name: 'Shillong', image: require('@/assets/images/cities/shillong.png'), color: 'standard' },
     { name: 'Bhubaneswar', image: require('@/assets/images/cities/bhubaneswar.png'), color: 'standard' },
   ];
 
